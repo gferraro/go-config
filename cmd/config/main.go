2538e1ea076ecf3115ee982812c627d1a0d86adc
Cameron Ryan-Pears <cameron.ryan.pears@gmail.com>
2019-09-10 17:25:23 +1200

    Added config migration from yaml to toml

---
+++ cmd/config/main.go
@@ -0,0 +1,278 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"reflect"
+	"time"
+
+	"github.com/alexflint/go-arg"
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v1"
+)
+
+var version = "<not set>"
+
+type Args struct {
+	MakeToml bool   `args:"--make-toml" help:"make toml config file from yaml config files"`
+	Dir      string `args:"--dir" help:"config directory"`
+}
+
+func (Args) Version() string {
+	return version
+}
+
+func procArgs() Args {
+	args := Args{
+		Dir: "/etc/cacophony",
+	}
+	arg.MustParse(&args)
+	return args
+}
+
+func main() {
+	if err := runMain(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runMain() error {
+	log.SetFlags(0)
+	log.Printf("running version: %s", version)
+	args := procArgs()
+
+	if args.MakeToml {
+		return makeToml(args)
+	}
+	return nil
+}
+
+func makeToml(args Args) error {
+	log.Println("making toml config")
+
+	v := viper.New()
+	v.SetConfigFile(path.Join(args.Dir, "config.toml"))
+
+	for _, f := range configProcessingFuncs {
+		s, err := f(args.Dir)
+		if err != nil {
+			return err
+		}
+		m, err := interfaceToMap(s)
+		if err != nil {
+			return err
+		}
+		if err := v.MergeConfigMap(m); err != nil {
+			return err
+		}
+	}
+	log.Printf("all settings: '%v'", v.AllSettings())
+	return v.WriteConfig()
+}
+
+func interfaceToMap(value interface{}) (m map[string]interface{}, err error) {
+	err = mapstructure.Decode(value, &m)
+	for k, v := range m {
+		if isSlice(v) && isStruct(reflect.ValueOf(v).Index(0)) {
+			s := reflect.ValueOf(v)
+			sliceMap := make([]map[string]interface{}, 0)
+			for i := 0; i < s.Len(); i++ {
+				m2, err := interfaceToMap(s.Index(i).Interface())
+				if err != nil {
+					return nil, err
+				}
+				sliceMap = append(sliceMap, m2)
+			}
+			m[k] = sliceMap
+		} else if isZeroVal(v) {
+			delete(m, k)
+		}
+	}
+	return
+}
+
+func isStruct(x interface{}) bool {
+	return reflect.ValueOf(x).Kind() == reflect.Struct
+}
+
+func isSlice(x interface{}) bool {
+	return reflect.ValueOf(x).Kind() == reflect.Slice
+}
+
+func isZeroVal(x interface{}) bool {
+	switch reflect.ValueOf(x).Kind() {
+	case reflect.Slice:
+		return false
+	case reflect.Map:
+		return false
+	}
+	return x == reflect.Zero(reflect.TypeOf(x)).Interface()
+}
+
+var configProcessingFuncs = []func(string) (interface{}, error){
+	processAttiny,
+	processModem,
+	processLepton,
+	processThermalRecorder,
+	processLocation,
+	processDevice,
+	processManagementd,
+}
+
+func processAttiny(configDir string) (interface{}, error) {
+	s := &rawAttinyConfig{}
+	return s, yamlToStruct(path.Join(configDir, "attiny.yaml"), s)
+}
+
+func yamlToStruct(configFile string, s interface{}) error {
+	log.Printf("reading '%v'", configFile)
+	buf, err := ioutil.ReadFile(configFile)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	return yaml.Unmarshal(buf, s)
+}
+
+type rawAttinyConfig struct {
+	PiWakeUp string         `yaml:"pi-wake-time" mapstructure:"windows.poweron"`
+	PiSleep  string         `yaml:"pi-sleep-time" mapstructure:"windows.poweroff"`
+	Voltages attinyVoltages `yaml:"voltages" mapstructure:"battery"`
+}
+
+type attinyVoltages struct {
+	Enable      bool   `yaml:"enable" mapstructure:"enable-voltage-readings"`
+	NoBattery   uint16 `yaml:"no-battery" mapstructure:"no-battery-voltage"`
+	LowBattery  uint16 `yaml:"low-battery" mapstructure:"low-battery-voltage"`
+	FullBattery uint16 `yaml:"full-battery" mapstructure:"full-battery-voltage"`
+}
+
+func processModem(configDir string) (interface{}, error) {
+	s := &rawModemdConfig{}
+	return s, yamlToStruct(path.Join(configDir, "modemd.yaml"), s)
+}
+
+type rawModemdConfig struct {
+	ModemsConfig      []modemConfig `yaml:"modems" mapstructure:"modems.list"`
+	TestInterval      time.Duration `yaml:"test-interval" mapstructure:"modemd.test-interval"`
+	PowerPin          string        `yaml:"power-pin" mapstructure:"gpio.modem-power"`
+	InitialOnTime     time.Duration `yaml:"initial-on-time" mapstructure:"modemd.initial-on-duration"`
+	FindModemTime     time.Duration `yaml:"find-modem-time" mapstructure:"modemd.find-modem-timeout"`
+	ConnectionTimeout time.Duration `yaml:"connection-timeout" mapstructure:"modemd.connection-timeout"`
+	RequestOnTime     time.Duration `yaml:"request-on-time" mapstructure:"modemd.request-on-duration"`
+}
+
+type modemConfig struct {
+	Name          string `yaml:"name" mapstructure:"name"`
+	Netdev        string `yaml:"netdev" mapstructure:"net-dev"`
+	VendorProduct string `yaml:"vendor-product" mapstructure:"vendor-product-id"`
+}
+
+func processLepton(configDir string) (interface{}, error) {
+	s := &rawLepton{}
+	return s, yamlToStruct(path.Join(configDir, "../leptond.yaml"), s)
+}
+
+type rawLepton struct {
+	SPISpeed    int64  `yaml:"spi-speed" mapstructure:"lepton.spi-speed"`
+	PowerPin    string `yaml:"power-pin" mapstructure:"gpio.thermal-camera-power"`
+	FrameOutput string `yaml:"frame-output" mapstructure:"lepton.frame-output"`
+}
+
+func processThermalRecorder(configDir string) (interface{}, error) {
+	s := &rawThermalRecorder{}
+	if err := yamlToStruct(path.Join(configDir, "../thermal-recorder.yaml"), s); err != nil {
+		return nil, err
+	}
+	var start, stop string
+	if s.Recorder.UseSunriseSunsetWindow {
+		start = (time.Minute * time.Duration(s.Recorder.SunsetOffset)).String()
+		stop = (time.Minute * time.Duration(s.Recorder.SunriseOffset)).String()
+	} else {
+		start = s.Recorder.WindowStart
+		stop = s.Recorder.WindowEnd
+	}
+	s.Recorder.RecordingStart = start
+	s.Recorder.RecordingStop = stop
+	return s, nil
+}
+
+type rawThermalRecorder struct {
+	OutputDir    string          `yaml:"output-dir" mapstructure:"recorder.output-dir"`
+	MinDiskSpace uint64          `yaml:"min-disk-space" mapstructure:"recorder.min-disk-space"`
+	Recorder     recorderConfig  `mapstructure:",squash"`
+	Motion       motionConfig    `mapstructure:",squash"`
+	Throttler    throttlerConfig `mapstructure:",squash"`
+}
+
+type recorderConfig struct {
+	MinSecs                int    `yaml:"min-secs" mapstructure:"recorder.min-secs"`
+	MaxSecs                int    `yaml:"max-secs" mapstructure:"recorder.max-secs"`
+	PreviewSecs            int    `yaml:"preview-secs" mapstructure:"recorder.preview-secs"`
+	UseSunriseSunsetWindow bool   `yaml:"sunrise-sunset" mapstructure:"-"`
+	SunriseOffset          int    `yaml:"sunrise-offset" mapstructure:"-"`
+	SunsetOffset           int    `yaml:"sunset-offset" mapstructure:"-"`
+	WindowStart            string `yaml:"window-start" mapstructure:"-"`
+	WindowEnd              string `yaml:"window-end" mapstructure:"-"`
+	RecordingStart         string `yaml:"-" mapstructure:"windows.start-recording"`
+	RecordingStop          string `yaml:"-" mapstructure:"windows.stop-recording"`
+}
+
+type motionConfig struct {
+	DynamicThreshold bool   `yaml:"dynamic-thresh" mapstructure:"motion-detection.min-secs"`
+	TempThresh       uint16 `yaml:"temp-thresh" mapstructure:"motion-detection.temp-thresh"`
+	DeltaThresh      uint16 `yaml:"delta-thresh" mapstructure:"motion-detection.delta-thresh"`
+	CountThresh      int    `yaml:"count-thresh" mapstructure:"motion-detection.count-thresh"`
+	FrameCompareGap  int    `yaml:"frame-compare-gap" mapstructure:"motion-detection.frame-compare-gap"`
+	UseOneDiffOnly   bool   `yaml:"one-diff-only" mapstructure:"motion-detection.use-one-diff-only"`
+	TriggerFrames    int    `yaml:"trigger-frames" mapstructure:"motion-detection.trigger-frames"`
+	WarmerOnly       bool   `yaml:"warmer-only" mapstructure:"motion-detection.warmer-only"`
+	EdgePixels       int    `yaml:"edge-pixels" mapstructure:"motion-detection.edge-pixels"`
+	Verbose          bool   `yaml:"verbose" mapstructure:"motion-detection.verbose"`
+}
+
+type throttlerConfig struct {
+	ApplyThrottling bool          `yaml:"apply-throttling" mapstructure:"throttle.active"`
+	BucketSize      time.Duration `yaml:"bucket-size" mapstructure:"throttle.bucket-size"`
+	MinRefill       time.Duration `yaml:"min-refill" mapstructure:"throttle.min-refill"`
+}
+
+func processLocation(configDir string) (interface{}, error) {
+	s := &rawLocation{}
+	return s, yamlToStruct(path.Join(configDir, "location.yaml"), s)
+}
+
+type rawLocation struct {
+	Latitude     float32   `yaml:"latitude" mapstructure:"locaiton.latitude"`
+	Longitude    float32   `yaml:"longitude" mapstructure:"locaiton.longitude"`
+	LocTimestamp time.Time `yaml:"timestamp" mapstructure:"locaiton.timestamp"`
+	Altitude     float32   `yaml:"altitude" mapstructure:"locaiton.altitude"`
+	Accuracy     float32   `yaml:"accuracy" mapstructure:"locaiton.accuracy"`
+}
+
+func processDevice(configDir string) (interface{}, error) {
+	s := &rawDeviceConfig{}
+	if err := yamlToStruct(path.Join(configDir, "device.yaml"), s); err != nil {
+		return nil, err
+	}
+	return s, yamlToStruct(path.Join(configDir, "device-priv.yaml"), s)
+}
+
+type rawDeviceConfig struct {
+	ServerURL  string `yaml:"server-url" mapstructure:"device.server"`
+	Group      string `yaml:"group" mapstructure:"device.group"`
+	DeviceName string `yaml:"device-name" mapstructure:"device.name"`
+	Password   string `yaml:"password" mapstructure:"device.password"`
+	DeviceID   int    `yaml:"device-id" mapstructure:"device.id"`
+}
+
+func processManagementd(configDir string) (interface{}, error) {
+	s := &rawManagementdConfig{}
+	return s, yamlToStruct(path.Join(configDir, "managementd.yaml"), s)
+}
+
+type rawManagementdConfig struct {
+	Port int `yaml:"port" mapstructure:"managementd.port"`
+}
