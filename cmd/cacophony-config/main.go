d1fab7715618ee07573e744cda829b495ba9b234
cam <cameron.ryan.pears@gmail.com>
2019-10-10 12:42:42 +1300

    Added args

--- cmd/cacophony-config/main.go
+++ cmd/cacophony-config/main.go
@@ -3,13 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"strings"
 
 	config "github.com/TheCacophonyProject/go-config"
+	"github.com/alexflint/go-arg"
 )
 
 var version = "<not set>"
+
+type Args struct {
+	ConfigDir string   `arg:"-c,--config" help:"path to configuration directory"`
+	Write     bool     `arg:"-w,--write" help:"write to config file"`
+	Input     []string `arg:"positional"`
+}
+
+func (Args) Version() string {
+	return version
+}
+
+func procArgs() Args {
+	var args Args
+	args.ConfigDir = config.DefaultConfigDir
+	arg.MustParse(&args)
+	return args
+}
 
 func main() {
 	if err := runMain(); err != nil {
@@ -18,16 +35,30 @@ func main() {
 }
 
 func runMain() error {
+	args := procArgs()
 	log.SetFlags(0)
 	log.Printf("running version: %s", version)
 
-	settings, err := getNewSettings(os.Args[1:])
+	if args.Write {
+		return writeNewSettings(&args)
+	}
+	return nil
+}
+
+type setting struct {
+	section string
+	field   string
+	value   string
+}
+
+func writeNewSettings(args *Args) error {
+	settings, err := getNewSettings(args.Input)
 	if err != nil {
 		return err
 	}
 	log.Printf("new settings: %+v", settings)
 
-	conf, err := config.New(config.DefaultConfigDir)
+	conf, err := config.New(args.ConfigDir)
 	if err != nil {
 		return err
 	}
@@ -55,12 +86,6 @@ func runMain() error {
 	}
 
 	return nil
-}
-
-type setting struct {
-	section string
-	field   string
-	value   string
 }
 
 func getNewSettings(args []string) ([]setting, error) {
